699622ef630edc405fe2ab6aedcefea21ac65d67
Arthur-Clarke <71024137+Arthur-Clarke@users.noreply.github.com>
2022-01-15 20:19:11 -0300

    Add files via upload

---
+++ PRO-c24-template2-main-main/PRO-c24-template2-main-main/Iron.js
@@ -0,0 +1,29 @@
+class Iron{
+	constructor(x,y)
+	{
+		var options={
+			restitution:0.8,
+			friction:0.9,
+			density:12
+		}
+        this.body = Bodies.rectangle(x, y, 50, 50, options);
+        this.width = 80;
+        this.height = 50;
+  
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var iron=this.body.position;		
+			push()
+			translate(iron.x, iron.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("grey");
+			rect(0,0,this.r, this.r);
+			pop()
+	}
+
+}
